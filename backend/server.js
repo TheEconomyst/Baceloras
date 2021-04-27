8d9bb736cfca122fdba569cfec153eb6d5618f73
unknown <arlauskaslukas@gmail.com>
2021-04-27 14:26:49 +0300

    nigeriu darba

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,61 @@
 var { graphql, buildSchema } = require("graphql");
 var express = require("express");
 var { graphqlHTTP } = require("express-graphql");
+var pgp = require("pg-promise")();
+const conn = {
+  host: "localhost",
+  port: 5432,
+  database: "bachelor",
+  user: "postgres",
+  password: "admin",
+};
+var db = pgp(conn);
 
 var schema = buildSchema(`
     type Query {
-        hello: String
+        roles: [Role]
+        users: [SistemosNaudotojas]
+    }
+    type Role {
+      id_role: Int
+    }
+    type SistemosNaudotojas {
+      vardas: String
+      pavarde: String
+      elpastas: String!
+      slaptazodis: String!
+      id_sistemosnaudotojas: Int!
     }
 `);
 
+async function getRoles() {
+  let returnable = [];
+  await db
+    .any("SELECT * FROM role")
+    .then(function (data) {
+      returnable = data;
+    })
+    .catch(function (error) {});
+  return returnable;
+}
+
+async function getUsers() {
+  let returnable = [];
+  await db
+    .any("SELECT * FROM sistemosnaudotojas")
+    .then(function (data) {
+      returnable = data;
+    })
+    .catch(function (error) {});
+  return returnable;
+}
+
 var root = {
-  hello: () => {
-    return "hello world";
+  roles: () => {
+    return getRoles();
+  },
+  users: () => {
+    return getUsers();
   },
 };
 
